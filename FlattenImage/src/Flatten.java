0233384ca5a7a9c0ba5774e867e6dc1717eafc87
Chase Peak <chasepeak@Chases-MacBook-Pro-3.local>
2020-03-05 09:52:38 -0800

    image flattening complete

---
+++ FlattenImage/src/Flatten.java
@@ -0,0 +1,58 @@
+import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+
+public class Flatten {
+
+	public static void main(String[] args) throws IOException {
+		File[] files = new File("./resized/").listFiles();
+		File output = new File("flattened.out");
+		BufferedWriter bw = new BufferedWriter(new FileWriter(output));
+		char prevChar = 'C';
+		Arrays.parallelSort(files);
+		
+		for (File file : files)
+		{
+			if (file.getName().charAt(0) != prevChar)
+			{
+				bw.write(file.getName().charAt(0) + "\n");
+				prevChar = file.getName().charAt(0);
+			}
+	        if (file.isFile())
+	        {
+	        	System.out.println("Processing image: " + file.getName());
+	        	BufferedImage image = ImageIO.read(file);
+	        	
+	        	for (int y = 2; y < (image.getHeight() - 2); y++)
+	        	{
+	    			for (int x = 2; x < (image.getWidth() - 2); x++)
+	    			{
+	    				if (Math.abs(image.getRGB(x, y)) > 1)
+	    					bw.write('1');
+	    				else
+	    					bw.write('0');
+	    			}
+	    		}
+	        	image.flush();
+	        	bw.write('\n');
+	        }
+	        bw.flush();
+	    }
+		bw.write('\0');
+		bw.close();
+		System.out.println("Image flattening complete");
+	}
+
+}
---
+++ FlattenImage/src/ResizeImage.java
@@ -0,0 +1,61 @@
+import java.awt.AlphaComposite;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
+import javax.imageio.ImageIO;
+
+public class ResizeImage {
+
+	public static void main(String[] args) throws IOException {
+		int i = 0, j = 0;
+		char prevChar = '\0', currentChar;
+		File folder = new File("../../images/");
+	    File[] listOfFiles = folder.listFiles();
+	    Arrays.parallelSort(listOfFiles);
+		System.out.println("Total No of Files:" + listOfFiles.length);
+		Image img = null;
+		BufferedImage tempPNG = null;
+		File newFilePNG = null;
+		for (;i < listOfFiles.length; i++)
+		{
+			if (listOfFiles[i].isFile())
+			{
+				currentChar = listOfFiles[i].getName().charAt(0);
+				if (currentChar != prevChar)
+				{
+					j = 0;
+					prevChar = currentChar;
+				}
+				System.out.println("Resizing file:" + listOfFiles[i].getName());
+				img = ImageIO.read(new File("../../images/" + listOfFiles[i].getName()));
+				tempPNG = resizeImage(img, 32, 32);
+				newFilePNG = new File("./resized/" + prevChar + "_" + String.format("%04d", j) + ".png");
+				ImageIO.write(tempPNG, "png", newFilePNG);
+				j++;
+			}
+		}
+		System.out.println("Resize job complete.");
+	}
+
+	/**
+	 * This function resize the image file and returns the BufferedImage object that can be saved to file system.
+	 */
+	public static BufferedImage resizeImage(final Image image, int width, int height) {
+        final BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        final Graphics2D graphics2D = bufferedImage.createGraphics();
+        graphics2D.setComposite(AlphaComposite.Src);
+        
+        //below three lines are for RenderingHints for better image quality at cost of higher processing time
+        graphics2D.setRenderingHint(RenderingHints.KEY_INTERPOLATION,RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        graphics2D.setRenderingHint(RenderingHints.KEY_RENDERING,RenderingHints.VALUE_RENDER_QUALITY);
+        graphics2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+        graphics2D.drawImage(image, 0, 0, width, height, null);
+        graphics2D.dispose();
+        return bufferedImage;
+    }
+}
